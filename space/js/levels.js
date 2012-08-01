791cac97b60e525618a1e490e86f0cde5417626b
Christopher Bennage <christopher@bennage.com>
2012-07-31 17:23:23 -0700

    moved level loading into new file

---
+++ space/js/levels.js
@@ -0,0 +1,57 @@
+﻿/// <reference path="Asteroid.js" />
+(function () {
+
+    'use strict';
+
+    var Asteroid = space.Asteroid;
+
+    function next(entities, resolution) {
+        var r = Math.random;
+        var asteroid_count = 15;
+
+        while (asteroid_count > 0) {
+            var a = new Asteroid();
+            a.x = parseInt(r() * resolution.width, 10);
+            a.y = parseInt(r() * resolution.height, 10);
+            a.amount = parseInt(r() * 2000, 10) + 200;
+            entities.push(a);
+            asteroid_count--;
+        }
+    }
+
+    //function loadLevel(done) {
+    //    console.log('load level');
+    //    var request = new XMLHttpRequest();
+    //    request.open('GET', '/level.json', false);
+    //    request.send(null);
+
+    //    if (request.status === 200) {
+    //        var responseText = JSON.stringify({
+    //            'things': [{
+    //                'type': 'asteroid',
+    //                'x': 100,
+    //                'y': 100,
+    //                'amount': 1000
+    //            }, {
+    //                type: 'asteroid',
+    //                x: 300,
+    //                y: 100,
+    //                amount: 500
+    //            }]
+    //        });
+    //        //var data = JSON.parse(request.responseText);
+    //        var data = JSON.parse(responseText);
+    //        money = 1000;
+    //        data.things.forEach(function (x) {
+    //            var a = new Asteroid();
+    //            a.hydrate(x);
+    //            entities.push(a);
+    //        });
+    //    }
+    //}
+
+    WinJS.Namespace.define('space.levels', {
+        next: next
+    });
+
+}());
