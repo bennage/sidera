423f710c9e6c41b8de19153ed8d4f5238ebfd28b
Christopher Bennage <christopher@bennage.com>
2012-11-26 18:42:20 -0800

    comments on the anim shim

--- sidera/requestAnimationFrame.js
+++ sidera/requestAnimationFrame.js
@@ -1,30 +1,45 @@
 (function() {
 
-    // Ensure that the `requestAnimationFrame` API is present as expected
+  // RequestAnimationFrame Shim
+  // --------------------------
 
-   var lastTime = 0;
-   var vendors = ['ms', 'moz', 'webkit', 'o'];
-   for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
-       window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame'];
-       window.cancelAnimationFrame = window[vendors[x] + 'CancelAnimationFrame'] || window[vendors[x] + 'CancelRequestAnimationFrame'];
-   }
+  // This code is derived from the starter framework discussed on:
+  // http://www.contrejour.ie/BehindTheScenes.html
+  // The direct link to download the kit is
+  // http://www.contrejour.ie/downloads/HTML5GameKit.zip
+  // Ensure that the `requestAnimationFrame` API is present as expected.
 
-   // if we are on a browser that doesn't implement requestAnimationFrame, we create our own 
-   // using setTimeout
-   if(!window.requestAnimationFrame) window.requestAnimationFrame = function(callback, element) {
-       var currTime = new Date().getTime();
-       var timeToCall = Math.max(0, 16 - (currTime - lastTime));
-       var id = window.setTimeout(function() {
-           callback(currTime + timeToCall);
-       }, timeToCall);
-       lastTime = currTime + timeToCall;
-       return id;
-   };
+  var lastTime = 0;
+  var vendors = ['ms', 'moz', 'webkit', 'o'];
+  // We'll iterate over known vendor prefixes and alias them to the
+  // primary names for the API.
+  for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+    window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame'];
+    window.cancelAnimationFrame = window[vendors[x] + 'CancelAnimationFrame'] || window[vendors[x] + 'CancelRequestAnimationFrame'];
+  }
 
-   // if we are on a browser that doesn't implement cancelAnimationFrame, we create our own 
-   // using clearTimeout
-   if(!window.cancelAnimationFrame) window.cancelAnimationFrame = function(id) {
-       window.clearTimeout(id);
-   };
+  // If `requestAnimationFrame` is not implemented create it using `setTimeout`.
+  if(!window.requestAnimationFrame) {
+
+    window.requestAnimationFrame = function(callback, element) {
+      var currTime = new Date().getTime();
+      var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+      var id = window.setTimeout(function() {
+        callback(currTime + timeToCall);
+      }, timeToCall);
+      lastTime = currTime + timeToCall;
+      return id;
+    };
+
+  }
+
+  // If `cancelAnimationFrame` is not implemented create it using `clearTimeout`.
+  if(!window.cancelAnimationFrame) {
+
+    window.cancelAnimationFrame = function(id) {
+      window.clearTimeout(id);
+    };
+
+  }
 
 }());
