f27c011aaca543493a15df8712ef685ca4cd212a
Christopher Bennage <christopher@bennage.com>
2012-12-15 18:11:18 -0800

    matching the game resolution to the available space

--- source/scripts/resolution.js
+++ source/scripts/resolution.js
@@ -1,9 +1,8 @@
 define(function() {
 
-	// var resolution = { height: window.innerHeight, width: window.innerWidth };
-	return {
-		height: 600,
-		width: 800
-	};
+    return {
+        height: window.innerHeight,
+        width: window.innerWidth
+    };
 
 });
