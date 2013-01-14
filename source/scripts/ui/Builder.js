ff47d7aa4bcc8ca06435a985878db05ec4cc797f
Christopher Bennage <christopher@bennage.com>
2013-01-13 19:03:28 -0800

    displays a menu (stub) when tap is released

--- source/scripts/ui/Builder.js
+++ source/scripts/ui/Builder.js
@@ -1,13 +1,18 @@
 define(function(require) {
 
 	var MapGrid = require('entities/MapGrid'),
+		tween = require('animation/tween'),
 		input = require('input/provider');
+
+	var menuWidth = 100;
 
 	var Builder = function(camera, gameObjects) {
 			this.camera = camera;
 			this.gameObjects = gameObjects;
 			this.cell = null;
 			this.isValid = true;
+
+			this.resetMenu();
 		};
 
 	function isInGameBounds(p) {
@@ -28,36 +33,73 @@ define(function(require) {
 
 	Builder.prototype.render = function(ctx) {
 
-		if(this.cell === null) return;
+		if(this.cell !== null) {
+			var coords = this.camera.project(this.cell);
+			var size = (MapGrid.cellSize * this.camera.scale);
 
-		var coords = this.camera.project(this.cell);
-		var size = (MapGrid.cellSize * this.camera.scale);
+			ctx.save();
 
-		ctx.save();
+			ctx.fillStyle = this.isValid ? 'rgba(0,255,0,0.3)' : 'rgba(255,0,0,0.3)';
+			ctx.translate(coords.x, coords.y);
+			ctx.fillRect(-size / 2, -size / 2, size, size);
 
-		ctx.fillStyle = this.isValid ? 'rgba(0,255,0,0.3)' : 'rgba(255,0,0,0.3)';
-		ctx.translate(coords.x, coords.y);
-		ctx.fillRect(-size / 2, -size / 2, size, size);
+			ctx.restore();
 
-		ctx.restore();
+		} else if(this.displayMenuAt != null) {
+
+			var coords = this.camera.project(this.displayMenuAt);
+			ctx.save();
+
+			ctx.fillStyle = 'rgba(0,0,255,0.7)';
+			ctx.translate(coords.x, coords.y);
+			ctx.beginPath();
+			ctx.arc(0, 0, this.menu.width, 0, Math.PI * 2, false);
+			ctx.fill();
+			ctx.restore();
+		}
+
 	};
 
-	Builder.prototype.update = function() {
+	Builder.prototype.update = function(elapsed) {
+		var lastCell = this.cell;
 		this.cell = null;
 
 		if(input.state.handled) return;
 
-		if(input.state.pointers.length === 1) {
+		if(this.displayMenuAt) {
+			if(this.menu.width < menuWidth) {
+				this.menu.width = this.menu.tween(elapsed);
+			}
+
+		}
+
+		if(this.isValid && !input.state.hasPointer && lastCell !== null) {
+			this.displayMenuAt = lastCell;
+		} else if(input.state.pointers.length === 1) {
+
 			var worldCoords = this.camera.toWorldSpace(input.state);
 			worldCoords.x = Math.round(worldCoords.x);
 			worldCoords.y = Math.round(worldCoords.y);
 
+			this.resetMenu();
+
 			if(isInGameBounds(worldCoords)) {
 				this.cell = worldCoords;
+
 				this.isValid = !isOverEntity(worldCoords, this.gameObjects.friendlies) && !isOverEntity(worldCoords, this.gameObjects.enviroment);
+				input.state.handled = true;
 			}
 
 		}
+	};
+
+	Builder.prototype.resetMenu = function() {
+		this.displayMenuAt = null;
+		this.menu = {
+			width: 0,
+			tween: tween(0, menuWidth, 300, tween.smooth)
+		};
+
 	};
 
 	return Builder;
