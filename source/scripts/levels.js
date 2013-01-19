bb5dd9a1f783862746d3d621816b96cabc5c2ca2
Christopher Bennage <christopher@bennage.com>
2013-01-19 14:56:28 -0800

    level logic that sends waves and has an initial setup

--- source/scripts/levels.js
+++ source/scripts/levels.js
@@ -6,11 +6,124 @@
         MapGrid = require('entities/MapGrid'),
         Asteroid = require('entities/Asteroid');
 
+    var Generator = require('entities/Generator'),
+        Turret = require('entities/Turret'),
+        Miner = require('entities/Miner');
+
     var state = {
         money: 12345
     };
 
-    state.update = function(elapse) {
+    var start = {
+        friendlies: [{
+            type: Generator,
+            as: {
+                x: 10,
+                y: 10
+            }
+        }, {
+            type: Miner,
+            as: {
+                x: 12,
+                y: 10
+            }
+        }, {
+            type: Turret,
+            as: {
+                x: 9,
+                y: 9
+            }
+        }, {
+            type: Turret,
+            as: {
+                x: 9,
+                y: 10
+            }
+        }, {
+            type: Turret,
+            as: {
+                x: 9,
+                y: 11
+            }
+        }],
+        enviroment: [{
+            type: Asteroid,
+            as: {
+                x: 12,
+                y: 9,
+                amount: 1000
+            }
+        }, {
+            type: Asteroid,
+            as: {
+                x: 12,
+                y: 12,
+                amount: 2000
+            }
+        }, {
+            type: Asteroid,
+            as: {
+                x: 14,
+                y: 9,
+                amount: 500
+            }
+        }],
+        waves: [{
+            after: 2,
+            send: [{
+                type: Fighter,
+                count: 10,
+                formation: triangle,
+                from: {
+                    x: 0,
+                    y: 0
+                }
+            }]
+        }, {
+            after: 5,
+            send: [{
+                type: Bomber,
+                count: 1,
+                from: {
+                    x: 0,
+                    y: 0
+                }
+            }]
+        }]
+    };
+
+    function triangle(entities, origin) {
+
+        var entity;
+        var offsetX = 0,
+            offsetY = 0;
+        var space = 1.75;
+
+        for(var i = entities.length - 1; i >= 0; i--) {
+            entity = entities[i];
+
+            entity.x = origin.x - offsetX * space;
+            entity.y = origin.y - offsetY * space;
+
+            offsetX++;
+
+            if(offsetX > offsetY) {
+                offsetX = 0;
+                offsetY++;
+            }
+        }
+    }
+
+    state.update = function(elapsed) {
+
+        this.timeUntilNextWave -= elapsed;
+
+        if(this.timeUntilNextWave <= 0) {
+            if(!this.waves[this.waveId]) return;
+            this.sendWave();
+            this.setupNextWave();
+        }
+
         if(keyboard.isKeyPressed(81)) {
             //q
             sendWaveOf(Fighter);
@@ -21,7 +134,43 @@
         }
     };
 
-    state.render = function(ctx) {};
+    state.render = function(ctx) {
+
+        if(this.levelOver) return;
+
+        ctx.fillStyle = 'yellow';
+        ctx.font = '18px sans-serif';
+        ctx.fillText('next wave in ' + Math.round(this.timeUntilNextWave / 1000), 500, 20);
+    };
+
+    state.sendWave = function() {
+        var wave = this.waves[this.waveId].send[0];
+        var queue = [];
+        var entity;
+        for(var i = wave.count; i > 0; i--) {
+            entity = new wave.type();
+            // entity.x = -1 - (i * 1.1);
+            // entity.y = -1 - (i * 1.1);
+            this.objects.enemies.push(entity);
+            queue.push(entity);
+        }
+
+        if(wave.formation) {
+            wave.formation(queue, wave.from);
+        }
+
+    }
+
+    state.setupNextWave = function() {
+        this.waveId++;
+
+        if(this.waveId < this.waveCount) {
+            this.timeUntilNextWave = this.waves[this.waveId].after * 1000;
+        } else {
+            this.levelOver = true;
+        }
+
+    }
 
     function sendWaveOf(type) {
         var now = new Date();
@@ -37,38 +186,28 @@
         }
     }
 
+    function addTo(definition) {
+        var entity = new definition.type();
+        entity.hydrate(definition.as);
+        this.push(entity);
+    }
+
     function next(objects) {
-        var r = Math.random;
-        var asteroid_count = 35;
 
-        while(asteroid_count > 0) {
-            var a = new Asteroid();
-            a.x = parseInt(r() * MapGrid.columns, 10);
-            a.y = parseInt(r() * MapGrid.rows, 10);
-            a.amount = parseInt(r() * 2000, 10) + 200;
-            objects.enviroment.push(a);
-            asteroid_count--;
-        }
+        start.friendlies.forEach(addTo.bind(objects.friendlies));
+        start.enviroment.forEach(addTo.bind(objects.enviroment));
 
-        var a = new Asteroid();
-        a.x = 0;
-        a.y = 0;
-        a.amount = 2000;
-        objects.enviroment.push(a);
+        objects.friendlies.forEach(function(friendly) {
+            friendly.context = state;
 
-        var a = new Asteroid();
-        a.x = 1;
-        a.y = 1;
-        a.amount = 2000;
-        objects.enviroment.push(a);
-
-        var a = new Asteroid();
-        a.x = MapGrid.columns;
-        a.y = MapGrid.rows;
-        a.amount = 2000;
-        objects.enviroment.push(a);
+        });
 
         state.objects = objects;
+        state.waves = start.waves;
+        state.waveId = 0;
+        state.timeUntilNextWave = state.waves[0].after * 1000;;
+        state.waveCount = state.waves.length;
+
         return state;
     }
 
